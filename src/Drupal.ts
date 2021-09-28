506b306136e6e794b12099864cf11a5af05edb00
Đông <dong.pd1994@outlook.com>
2021-09-28 16:01:32 +0700

    Add files via upload

--- src/Drupal.ts
+++ src/Drupal.ts
@@ -1,7 +1,3 @@
-/**
- * @module Drupal
- */
-
 import {
   Configuration,
   ConfigurationInterface,
@@ -9,24 +5,30 @@ import {
 } from './Configuration';
 import { API, APIInterface } from 'src/api/API';
 import { EntityStorage } from 'src/schemes/entity/Entity';
+import { UserInfoInterface } from 'src/schemes/drupal/User';
+import { AuthProviderInterface } from 'src/authentication/BaseAuth';
+import { MainAuth } from 'src/authentication/MainAuth';
 import { RequestParams as RequestParamsType } from 'src/schemes/request/Request';
 import _ from 'lodash';
+
+interface AuthenticationInjectInterface {
+  api: APIInterface;
+}
 
 export class Drupal {
   public config: ConfigurationInterface;
   public api: APIInterface;
+  public auth: AuthProviderInterface;
   public editableProps: {};
 
   /**
    * Constructor for the Connection class.
-   *
-   * @param  {ConfigurationOptionsInterface} options
-   *   The options to process.
+   * @param options The options to process.
    */
-  constructor(options: ConfigurationOptionsInterface) {
+  constructor(options: ConfigurationOptionsInterface, { api }: AuthenticationInjectInterface,) {
     this.config = new Configuration(options);
     this.api = new API(this.config);
-
+    this.auth = new MainAuth(api);
     this.editableProps = {
       config: this.config
     };
@@ -34,17 +36,29 @@ export class Drupal {
 
   /**
    * Call API Drupal
-   *
-   * @param  {string} entityType
-   *   The entity type.
-   * @param  {QueryParamsType} params
-   *   The query parameters to process.
-   *
-   * @return {EntityStorageInterface}
-   *   The Prepared API Object.
-   *
+   * @param entityType The entity type.
+   * @param params The query parameters to process.
+   * @returns The Prepared API Object.
    */
   public callAPI(entityType: string, params: RequestParamsType = {}) {
     return new EntityStorage(entityType, this.api, this.config, params);
+  }
+
+  /**
+   * Login request to Drupal
+   * @param user The username of the user.
+   * @param password The password of the user.
+   * @returns The promise of the api request.
+   */
+  public loginDrupal(user: string, password: string, rememberMe: boolean): Promise<UserInfoInterface> {
+    return this.auth.login(user, password, rememberMe);
+  }
+
+  /**
+   * Logout request to Drupal.
+   * @returns The promise of the api request.
+   */
+  public logoutDrupal(): Promise<any> {
+    return this.auth.logout();
   }
 }
