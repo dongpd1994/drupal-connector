20d32ee62374af0dee24243c56b11189947a6081
Đông <dong.pd1994@outlook.com>
2021-10-04 11:08:23 +0700

    Add files via upload

---
+++ src/schemes/response/Response.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type DesTypeObjInterdace = object | null;
+export interface ResponseNodeByRouterInterface {
+  router: DesTypeObjInterdace,
+  includeField: DesTypeObjInterdace,
+  data: DesTypeObjInterdace
+}
